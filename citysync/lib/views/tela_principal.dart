dc58179c3537df4aff1bb1e1f7805e890594f044
WsylasD <168680397+WsylasD@users.noreply.github.com>
2025-10-24 09:47:11 -0300

    att modal name

--- citysync/lib/views/tela_principal.dart
+++ citysync/lib/views/tela_principal.dart
@@ -1,4 +1,4 @@
-import 'package:citysync/widgets/modal_pagina_inicial.dart';
+import 'package:citysync/widgets/modalpaginainicial.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:citysync/views/perfil.dart';
